b25623ce8480c547c627e083d6154d22cf5ad09f
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-07 22:16:33 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -54,7 +54,10 @@ class Dog
 
   def self.new_from_db(row)
     Dog.new(name: row[1], breed: row[2], id: row[0])
-    
   end
 
+  def self.find_by_name(name)
+    row = DB[:conn].execute("select * from dogs where name = #{name}").flatten
+    Dog.new_from_db(row)
+  end
 end
