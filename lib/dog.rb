b48e8d8079b9971810577a87e4bd64f095460c9c
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-07 21:54:51 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -46,9 +46,9 @@ class Dog
       Dog.create(name: name, breed: breed)
     else
       #return dog
-      Dog.new(name: row[1], breed: row[2], id: row[0])
+      #Dog.new(name: row[1], breed: row[2], id: row[0])
       row.flatten
-      binding.pry
+      # binding.pry
 
     end
     #binding.pry
