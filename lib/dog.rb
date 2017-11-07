42b6a9d5bea78c6de284df6a9e373f4b21376df4
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-07 21:35:01 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -32,7 +32,7 @@ class Dog
 
   def self.find_by_id(id)
     new_id = DB[:conn].execute("select * from dogs where id = #{id}").flatten
-    dog = Dog.new(new_id[1])
+    dog = Dog.new(name: new_id[1])
     binding.pry
   end
 
