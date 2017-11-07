67ed2fae8fd2a33cb6758e3baa36915f61532efc
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-07 21:11:13 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,10 +2,10 @@ require 'pry'
 class Dog
 
   attr_accessor :name, :breed, :id
-  
+
   def initialize(name, breed, id=nil)
-    @name = name 
-    @breed = breed 
+    @name = name
+    @breed = breed
     @id = id
   end
 end
