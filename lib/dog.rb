a461cd5c7a9686e8794f157b61d4642bbd3a409b
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-07 22:08:05 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -21,7 +21,7 @@ class Dog
   def save
     #binding.pry
     DB[:conn].execute("insert into dogs(name, breed) values ('#{self.name}', '#{self.breed}')")
-    @id = DB[:conn].execute("select id from dogs where name='#{self.name}'").flatten.join.to_i
+    @id = DB[:conn].execute("select id from dogs where name='#{self.name}'").flatten[0]
     self
   end
 
@@ -48,7 +48,6 @@ class Dog
       #return dog
       Dog.new(name: row[1], breed: row[2], id: row[0])
       # binding.pry
-
     end
     #binding.pry
 
