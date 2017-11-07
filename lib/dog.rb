8478ccd2674b9ba3cd12fc793e01355c051b0c9a
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-07 21:11:07 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,11 @@
 require 'pry'
 class Dog
 
-  def initialize(hsh)
-    binding.pry
+  attr_accessor :name, :breed, :id
+  
+  def initialize(name, breed, id=nil)
+    @name = name 
+    @breed = breed 
+    @id = id
   end
 end
