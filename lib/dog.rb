d7d7e5bf8699818d69667634ce6e32f9cf49d745
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-07 22:15:06 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -53,8 +53,8 @@ class Dog
   end
 
   def self.new_from_db(row)
-    dog = Dog.new(name: row[1], breed: row[2], id: row[0])
-    dog
+    Dog.new(name: row[1], breed: row[2], id: row[0])
+    
   end
 
 end
